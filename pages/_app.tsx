6bfeb4f6f652347fcf300188bb63952094d4a6d8
Khaled <khaled.mourraad@gmail.com>
2025-08-21 15:49:59 +0300

    chore: basic _app setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
+import LandingPage from "@/components/LandingPage";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { wrapper } from '@/store';
+import { Provider } from 'react-redux';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({ Component,  ...rest }: AppProps) {
+   const { store, props } = wrapper.useWrappedStore(rest);
+  return (
+    <Provider store={store}>
+
+    <LandingPage>
+      <Component {...props.pageProps} />;
+    </LandingPage>
+    </Provider>
+  )
 }
