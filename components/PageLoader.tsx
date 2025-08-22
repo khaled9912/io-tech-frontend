9d352945fa3c05715fcde5c60e313dbb1fcb498b
Khaled <khaled.mourraad@gmail.com>
2025-08-22 09:40:30 +0300

    feat: add pageloader component

---
+++ components/PageLoader.tsx
@@ -0,0 +1,7 @@
+const PageLoader = () => (
+  <div className="fixed inset-0 z-50 flex items-center justify-center bg-white">
+    <div className="h-16 w-16 animate-spin rounded-full border-b-4 border-t-4 border-gray-700"></div>
+  </div>
+);
+
+export default PageLoader;
