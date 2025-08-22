39d2abd227be7d1864e9ba4065e0bf8ea1b0a8ff
Khaled <khaled.mourraad@gmail.com>
2025-08-22 09:40:50 +0300

    feat: add search slider component

---
+++ components/Slider.tsx
@@ -0,0 +1,123 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { fetchSlides, selectSlides } from "@/store/slices/heroSlice";
+import { useSelector } from "react-redux";
+import { useAppDispatch } from "@/store";
+import { ISlide } from "@/types";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
+interface SliderProps {
+  initialSlides: ISlide[];
+}
+
+const Slider = ({ initialSlides }: SliderProps) => {
+  const [current, setCurrent] = useState(0);
+  const slidesFromRedux = useSelector(selectSlides);
+  const slides =
+    Number(slidesFromRedux?.length) > 0 ? slidesFromRedux : initialSlides;
+  const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    dispatch(fetchSlides()).finally(() => setLoading(false));
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (!slides || slides.length === 0) return;
+    const interval = setInterval(() => {
+      setCurrent((prev) => (prev === slides.length - 1 ? 0 : prev + 1));
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [slides]);
+
+  const prevSlide = () =>
+    setCurrent(current === 0 ? Number(slides?.length) - 1 : current - 1);
+  const nextSlide = () =>
+    setCurrent(current === Number(slides?.length) - 1 ? 0 : current + 1);
+
+  if (loading || !slides || slides.length === 0) {
+    return (
+      <div className="flex h-[calc(100vh-80px)] items-center justify-center">
+        <div className="h-12 w-12 animate-spin rounded-full border-b-4 border-t-4 border-primary"></div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="relative h-screen w-full overflow-hidden">
+      {/* Slider container */}
+      <div className="relative h-screen w-full overflow-hidden">
+        {/* Slides */}
+        <div
+          className="flex h-full w-max transition-all duration-1000 ease-in-out"
+          style={{ transform: `translateX(-${current * 100}vw)` }}
+        >
+          {slides.map((slide, idx) => (
+            <div
+              className="flex h-full w-screen flex-col gap-16 xl:flex-row"
+              key={idx}
+            >
+              <div className="relative h-full w-full">
+                <Image
+                  src={(slide as any).url}
+                  alt="slider image"
+                  fill
+                  sizes="100%"
+                  className="object-cover"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+
+        {/* Left/Right arrows */}
+        <button
+          onClick={prevSlide}
+          className="absolute left-4 top-1/2 -translate-y-1/2 rounded-full bg-gray-700 bg-opacity-50 p-2 text-white transition hover:bg-opacity-80"
+        >
+          <ChevronLeft />
+        </button>
+        <button
+          onClick={nextSlide}
+          className="absolute right-4 top-1/2 -translate-y-1/2 rounded-full bg-gray-700 bg-opacity-50 p-2 text-white transition hover:bg-opacity-80"
+        >
+          <ChevronRight />
+        </button>
+
+        <div className="absolute left-40 top-3/4 flex -translate-y-1/2 flex-col gap-4">
+          <button className="rounded bg-white p-4">Read More</button>
+        </div>
+        <div className="absolute right-40 top-3/4 flex -translate-y-1/2 flex-col gap-4 max-sm:hidden">
+          <Image
+            src={"/avatar.png"}
+            alt="Slide Logo"
+            width={200}
+            height={200}
+            className="object-contain"
+          />
+        </div>
+
+        {/* Vertical dots */}
+        <div className="absolute left-4 top-3/4 flex -translate-y-1/2 flex-col gap-4">
+          {slides.map((_, index) => (
+            <div
+              key={index}
+              className={`flex h-2 w-2 cursor-pointer items-center justify-center rounded-full ring-1 ring-white ${
+                current === index ? "scale-150" : ""
+              }`}
+              onClick={() => setCurrent(index)}
+            >
+              {current === index && (
+                <div className="h-[6px] w-[6px] rounded-full bg-white"></div>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
